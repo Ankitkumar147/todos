e54488f19be7490e0c4b399e45ac54a182ee58a3
Ankitkumar147 <aankitchouhan3@gmail.com>
2023-08-31 23:21:29 +0530

    added signup page with the css for better look

--- public/loginservices.js
+++ public/loginservices.js
@@ -1,8 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const loginButton = document.querySelector(".login-button");
-    const inputFields = document.querySelector(".input-fields");
+  const loginButton = document.querySelector(".login-button");
+  const signupButton = document.querySelector(".signup-button");
+  const loginFields = document.querySelector(".login-input-fields");
+  const signupFields = document.querySelector(".signup-input-fields");
 
-    loginButton.addEventListener("click", function() {
-      inputFields.style.display = "block";
-    });
+  loginButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    loginFields.style.display = "block";
+    signupFields.style.display = "none";
   });
+
+  signupButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    signupFields.style.display = "block";
+    loginFields.style.display = "none";
+  });
+});
+
